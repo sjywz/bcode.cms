a9c6f71ee1de3bb9eefdf0f3774d693bd27cab60
yy <yy@orb.com>
2024-07-06 09:08:00 +0800

    修改csp

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,32 @@
+const assetsSrc = [
+  "'self'",
+  'data:',
+  'blob:',
+  'market-assets.strapi.io',
+  'cms.yousyou.xyz',
+];
+
 module.exports = [
   'strapi::errors',
-  'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
-  'strapi::favicon',
   'strapi::public',
+  'strapi::favicon',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': assetsSrc,
+          'media-src': assetsSrc,
+        }
+      }
+    }
+  }
 ];
